19dc50ecc7a152fea780b5ccbf49de396a407e25
Amanda <amandaduuaarte@gmail.com>
2021-08-26 12:04:53 -0300

    ajustes no script

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,73 @@
-let tempValue = 0;
-let type;
-let number;
-let final
-let valor
 
-function transform (tempValue, type, number, final,valor){
-    let total
-    tempValue = document.querySelector("#tempValue")
-    type = document.getElementById("type"); 
-    number = tempValue.value
-    valor = type.value
+let tempValue ;
+let type ;
+let final;
 
-    if (valor = "celsius"){
-        total = (number *9/5 + 32)  + " em celsius"
-    }else if (valor = "fahrenheit"){
-        total = ((number -32) *5/2) + " em fahrenheit "
-    }else{
-        console.log("erro na conversão ")
+function transform(tempValue, type){
+    tempValue = document.querySelector('#tempValue');
+    type = document.querySelector('#type');
+
+    const celsius = type.value.toUpperCase().includes('C');
+    const fahrenheit = type.value.toUpperCase().includes('F');
+    let valor 
+    if (!celsius && !fahrenheit){
+       valor = console.log("error")
+    }else {
+        if (celsius){
+            valor = ((tempValue.value * 9/5) + 32 ) + " em fahrenheit"
+        }else if (fahrenheit){
+            valor = ((tempValue.value -32) * 5/9) + " em celsius "
+        }
+        
+        final =  document.querySelector("#final").innerHTML=`A temperatura é: ${valor} `
+          return final
     }
-   final =  document.querySelector("#final").innerHTML=`A temperatura é: ${total} `
-    return final
-    // console.log( `A temperatura é: ${total} `)
-    // return console.log(tempValue.value);
+    
+    return valor
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function transform (tempValue, type, number, final,valor){
+//     let total
+//     tempValue = document.querySelector("#tempValue")
+//     type = document.querySelector("#type"); 
+//     number = tempValue.value
+//     valor = type.value
+
+//     if (valor = "C"){
+//         total = (number *9/5 + 32)  + " em fahrenheit"
+//     }else if (valor = "F"){
+//         total = ((number -32) *5/2) + " em celsius "
+//     }else{
+//         console.log("erro na conversão ")
+//     }
+//   // final =  document.querySelector("#final").innerHTML=`A temperatura é: ${total} `
+//     //return final
+//      console.log( `A temperatura é: ${total} `)
+//     // return console.log(tempValue.value);
+// }
