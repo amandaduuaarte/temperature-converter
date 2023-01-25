372cf038648242ea23a0bd68ea923c084a718700
Amanda Duarte <amandaduuaarte@gmail.com>
2023-01-24 23:47:02 -0300

    refactor: Adjustings in style and script code

--- JS/script.js
+++ JS/script.js
@@ -1,124 +1,70 @@
+const FinalValue = (valor, type) => {
+  document.querySelector(
+    "#final"
+  ).innerHTML = `A temperatura é:  ${valor}° ${type} `;
+};
 
-let tempValue ;
-let type ;
-let final;
-let tempValueKelvin;
-let typeKelvin;
-// adicionar o toFixed
-function transform(tempValue, type){
-    tempValue = document.querySelector('#tempValue');
-    type = document.querySelector('#type');
+const getInputTempValues = (nameInput, nameCheckbox) => {
+  const getTempValue = document.querySelector(`#${nameInput}`).value;
+  const tempValue = parseInt(getTempValue);
 
-    const celsius = type.value.toUpperCase().includes('C');
-    const fahrenheit = type.value.toUpperCase().includes('F');
-    const numberFahrenheit = parseInt(tempValue.value);
-    let valor
-    
-    if (!celsius && !fahrenheit){
-       valor = console.log("error")
-    }else {
-        if (celsius){
-            valor = (((numberFahrenheit* 1.8) + 32 ).toFixed('2')) + " em fahrenheit"
-        }else if (fahrenheit){
-            valor = (((numberFahrenheit -32) * 1.8).toFixed('2')) + " em celsius "
-        }
-        
-        final =  document.querySelector("#final").innerHTML=`A temperatura é: ${valor} `
-          return final
-    }
-    
-    return valor
-}
-function transformKelvin (tempValueKelvin, typeKelvin){
-    tempValueKelvin = document.querySelector('#tempValueKelvin');
-    typeKelvin = document.querySelector('#typeKelvin');
+  const type = document.querySelector(
+    `input[name=${nameCheckbox}]:checked`
+  ).value;
 
-    const kelvin = typeKelvin.value.toUpperCase().includes('K');
-    const celsius = typeKelvin.value.toUpperCase().includes('C');
-    const numberKelvin = parseInt(tempValueKelvin.value);
-    let valor 
-    if (!celsius && !kelvin){
-        valor = console.log("error")
-     }else {
-         if (celsius){
-             valor = (((numberKelvin)  + 273 ).toFixed('2')) + " em kelvin"
-         }else if (kelvin){
-             valor = (((numberKelvin) - 273).toFixed('2')) + " em celsius "
-         }
-         
-         final =  document.querySelector("#final").innerHTML=`A temperatura é: ${valor} `
-           return final
-     }
-     
-     return valor
+  return {
+    tempValue,
+    type,
+  };
+};
+
+function transformCelsiusToFahrenheit() {
+  const { tempValue, type } = getInputTempValues(
+    "tempValueCelsiusToFahrenheit",
+    "celsiusToFahrenheit"
+  );
+
+  if (!type) {
+    document.querySelector("#final").innerHTML = "Valor inválido";
+  } else if (type === "celcius") {
+    const value = Math.floor(tempValue * 1.8 + 32);
+    FinalValue(value, "Fahrenheit");
+  } else if (type === "fahrenheit") {
+    const value = Math.floor((tempValue - 32) * 1.8);
+    FinalValue(value, "Celcius");
+  }
 }
 
-function kelvinFahrenheit(tempValueFahrenheit, typeFahrenheit){
-    tempValueFahrenheit = document.querySelector('#tempValueFahrenheit');
-    typeFahrenheit = document.querySelector('#typeFahrenheit');
+function transformCelsiusToKelvin() {
+  const { tempValue, type } = getInputTempValues(
+    "tempValueKelvinToFahrenheit",
+    "celsiusToKelvin"
+  );
 
-    const kelvin = typeFahrenheit.value.toUpperCase().includes('K');
-    const fahrenheit = typeFahrenheit.value.toUpperCase().includes('F');
-    let valor ;
-    const numberFahrenheit = parseInt(tempValueFahrenheit.value);
-    if (!fahrenheit && !kelvin){
-        valor = console.log("error")
-     }else {
-         if (kelvin){
-             valor = ((((numberFahrenheit) * 1.8 ) - 459.67).toFixed('2')) + " em fahrenheit"
-         }else if (fahrenheit){
-             valor = ((((numberFahrenheit) + 459.67) / 1.8 ).toFixed('2')) + " em kelvin "
-         }
-         
-         final =  document.querySelector("#final").innerHTML=`A temperatura é: ${valor} `
-           return final
-     }
-     
-     return valor
-
+  if (!type) {
+    document.querySelector("#final").innerHTML = "Valor inválido";
+  } else if (type === "celcius") {
+    const value = Math.floor(tempValue + 273);
+    FinalValue(value, "kelvin");
+  } else if (type === "kelvin") {
+    const value = Math.floor(tempValue - 273);
+    FinalValue(value, "Celcius");
+  }
 }
 
+function transformKelvinToCelcius() {
+  const { tempValue, type } = getInputTempValues(
+    "tempValueKelvinToFahrenheit",
+    "kelvinToFahrenheit"
+  );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function transform (tempValue, type, number, final,valor){
-//     let total
-//     tempValue = document.querySelector("#tempValue")
-//     type = document.querySelector("#type"); 
-//     number = tempValue.value
-//     valor = type.value
-
-//     if (valor = "C"){
-//         total = (number *9/5 + 32)  + " em fahrenheit"
-//     }else if (valor = "F"){
-//         total = ((number -32) *5/2) + " em celsius "
-//     }else{
-//         console.log("erro na conversão ")
-//     }
-//   // final =  document.querySelector("#final").innerHTML=`A temperatura é: ${total} `
-//     //return final
-//      console.log( `A temperatura é: ${total} `)
-//     // return console.log(tempValue.value);
-// }
+  if (!type) {
+    document.querySelector("#final").innerHTML = "Valor inválido";
+  } else if (type === "kelvin") {
+    const value = Math.floor(tempValue * 1.8 - 459.67);
+    FinalValue(value, "Fahrenheit");
+  } else if (type === "Fahrenheit") {
+    const value = Math.floor((tempValue + 459.67) / 1.8);
+    FinalValue(value, "kelvin");
+  }
+}
